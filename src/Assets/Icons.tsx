1132d1f0c02a8e4d08ccef1c0f001ae9524cbf07
Sounish Nath <flock.sinasini@gmail.com>
2021-07-12 01:16:09 +0530

    button CSS

--- src/Assets/Icons.tsx
+++ src/Assets/Icons.tsx
@@ -45,3 +45,21 @@ export const FileTextIcon = ({
     <path d="M14 3v5h5M16 13H8M16 17H8M10 9H8" />
   </svg>
 );
+
+export const AlertIcon = (): JSX.Element => (
+  <svg
+    className="h-6 w-6 text-red-600"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    aria-hidden="true"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+    />
+  </svg>
+);

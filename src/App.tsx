fc98170560e5c734cab416a8f0e9dd626c4da5b2
Sounish Nath <flock.sinasini@gmail.com>
2021-07-11 11:16:07 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { Header } from "./components";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App p-2 bg-gray-50 h-screen">
       <Header />
